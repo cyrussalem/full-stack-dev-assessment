96ada392af8596d493c850f3fa13687e169b6f2e
samruddhinangare <samruddhinangare@gmail.com>
2024-03-10 22:07:58 +0530

    http get call code done

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const fs = require('fs').promises;
 let counter = 1;
 const TAXABLE_ITEMS_FILE = 'data.csv';
 const UPDATE_INTERVAL = 1000;
+var MyData = [];
+var http = require('http'); //for loading http server 
 
 // Function to add a new line to a file
 async function addLineToFile(filename, line) {
     try {
         // Read the existing content of the file
         let data = await fs.readFile(filename, 'utf8');
+        readData(line) 
 
         // Append the new line
         const newData = data + '\n' + line;
 
         // Write the updated content back to the file
-        await  fs.writeFile(filename, newData);
+        await fs.writeFile(filename, newData);
 
         console.log(`${TAXABLE_ITEMS_FILE} updated`)
     } catch (err) {
@@ -27,12 +30,13 @@ async function clearFile(filename) {
         await fs.writeFile(filename, 'Item, Cost, Tax, Total');
 
         console.log('Cleared file');
+
     } catch (err) {
         console.error('Error writing file:', err);
     }
 }
 
-async function start () {
+async function start() {
     await clearFile(TAXABLE_ITEMS_FILE);
 
     setInterval(async () => {
@@ -44,4 +48,28 @@ async function start () {
     }, UPDATE_INTERVAL);
 }
 
+//function to create object dynamically
+function test(value) {
+    this.Item = value[0];
+    this.Cost = value[1];
+    this.Tax = value[2];
+    this.Total = value[3];
+};
+
+// function to add the newly added line to create a data object into array of Object
+function readData(data) {
+    let dataArray = data.split(', ');
+    MyData.push(new test(dataArray));
+}
+
+// Create a webserver with a request listener callback.  This will write the response header with the content type as json, and end the response by sending the array in JSON format.
+function server() {
+    var server = http.createServer(function (req, resp) {
+        resp.writeHead(200, { 'content-type': 'application/json' });
+        resp.end(JSON.stringify(MyData));
+    });
+    server.listen(8080);
+}
+
 start();
+server();
