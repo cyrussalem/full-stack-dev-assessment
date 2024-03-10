a09faaba17c75a71a7632ea352a1d279e683cb8d
samruddhinangare <samruddhinangare@gmail.com>
2024-03-11 01:38:34 +0530

    Handle error condition for angular code

--- angular-code/src/app/view-table/view-table.component.ts
+++ angular-code/src/app/view-table/view-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReadDataService } from '../../services/read-data.service';
 import { TableData } from '../../model/table-data.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-table',
@@ -9,14 +10,20 @@ import { TableData } from '../../model/table-data.model';
 })
 export class ViewTableComponent implements OnInit{
   TableValues: TableData[] = [];
+  isError: boolean = false;
 
  constructor(private readDataService: ReadDataService) {}
 
  //used to read data from the localhost server
   ngOnInit(): void {
     setInterval(() => {
-      this.readDataService.getTableData().subscribe((data: any) =>  {
-        this.TableValues = data;
+      this.readDataService.getTableData().subscribe((data: TableData[]) =>  {
+        if(data) {
+          this.TableValues = data;
+          this.isError = false
+        }
+      }, (error: HttpErrorResponse) => {
+        this.isError = true
       });
     }, 1000);
   }
